bfe30f6e9d0a5d4efdffdde730af4826406c90b4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-23 17:10:57 -0500

    Add test cases for groupOperation

--- spec/models/group_operation_spec.rb
+++ spec/models/group_operation_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe GroupOperation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before :all do 
+    User.destroy_all
+    user = create :user, :email
+    @group = create :group, author: user
+    @operation = create :operation, author: user 
+  end
+
+  subject { GroupOperation.new( group: @group, operation: @operation ) }
+
+  it 'Should have a group' do 
+    subject.group = nil
+    expect(subject).to_not be_valid 
+  end
+
+  it 'Should have an operation' do 
+    subject.operation = nil
+    expect(subject).to_not be_valid
+  end
 end
