234f7092b1cc9af8431f437b44709fbe8a89d158
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-24 18:01:42 -0500

    Add test cases for the categories pages

--- spec/integration/categories_spec.rb
+++ spec/integration/categories_spec.rb
@@ -50,4 +50,64 @@ RSpec.describe 'Categories integration', type: :feature do
       expect(page).to have_current_path(categories_path)
     end
   end
+
+  describe 'Categories page' do
+    before :all do 
+      Group.destroy_all
+      groups = create_list :group, 20, author: @user
+      operations = create_list :operation, 20, author: @user
+      groups.each_with_index do |group, index|
+        GroupOperation.create(group: group, operation: operations[index])
+      end
+    end
+
+    before :each do
+      visit new_user_session_path
+      fill_in('Email', with: @user.email)
+      fill_in('Password', with: @user.password)
+      click_button 'Log in'
+      visit categories_path
+    end
+
+
+    it 'should have an icon for each group' do
+      container = page.find('.groups')
+      images = container.find_all('img')
+
+      expect(images.length).to eq(20)
+    end
+
+    it 'should have the name of each category' do
+      groups = Group.all 
+
+      groups.each do |group|
+        expect(page).to have_content(group.name)
+      end
+    end
+
+    it 'should have the total ammount of each category' do
+      groups = Group.all 
+
+      groups.each do |group|
+        expect(page).to have_content("$#{group.total_amount}")
+      end
+    end
+
+    it 'should link to the transaction page of the category' do
+      group_record = Group.first
+      group = page.find('a', id: "Group-#{group_record.id}")
+
+      group.click
+
+      expect(page).to have_current_path(category_transactions_path(category_id: group_record.id))
+    end
+
+    it 'should have a new category button' do
+      add_new_category = page.find_link('Add New Category')
+
+      add_new_category.click
+
+      expect(page).to have_current_path(new_category_path)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { build :user, :email }
-
-  it 'should have name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  describe 'Validations' do
+    subject { build :user, :email }
+  
+    it 'should have name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it 'should have a name longer than two characters' do
+      subject.name = 'N'
+      expect(subject).to_not be_valid
+    end
+  
+    it 'should have an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it 'should have a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'should have a name longer than two characters' do
-    subject.name = 'N'
-    expect(subject).to_not be_valid
-  end
+  describe 'Methods' do
+    before :all do 
+      User.destroy_all
+      @user = create :user, :email
+      create_list :group, 20, author: @user
+    end
 
-  it 'should have an email' do
-    subject.email = nil
-    expect(subject).to_not be_valid
-  end
+    it 'should return the list of categories for the user' do
+      groups = @user.ordered_groups
 
-  it 'should have a password' do
-    subject.password = nil
-    expect(subject).to_not be_valid
+      expect(groups.length).to eq(20)
+    end
   end
 end
