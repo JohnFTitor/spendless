02ec53fb9192225ffc4c50152af40176bef83d40
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-24 11:55:02 -0500

    Define test cases for categories integration

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe "Categories", type: :request do
       get categories_path
 
       expect(response).to have_http_status(:found)
-      expect(response).to redirect_to(splash_path)
+      expect(response).to redirect_to(welcome_path)
     end
 
     it 'Should redirect to splash screen if not authenticaded with root path' do 
       get '/'
 
       expect(response).to have_http_status(:found)
-      expect(response).to redirect_to(splash_path)
+      expect(response).to redirect_to(welcome_path)
     end
   end
 end
