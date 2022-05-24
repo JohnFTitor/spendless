b16e5567f4f97d59956518db61ac1b64fa1a3667
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-24 10:04:07 -0500

    Add test cases for group_operation

--- spec/models/group_operation_spec.rb
+++ spec/models/group_operation_spec.rb
@@ -20,4 +20,35 @@ RSpec.describe GroupOperation, type: :model do
     subject.operation = nil
     expect(subject).to_not be_valid
   end
+
+  it 'should increment total amount of group if created' do 
+    user = create :user, :email
+    group = create :group, author: user
+    operation1 = create :operation, author: user
+    operation2 = create :operation, author: user
+    total = operation1.amount + operation2.amount
+
+    GroupOperation.create( group: group, operation: operation1)
+    GroupOperation.create( group: group, operation: operation2)
+
+    expect(group.total_amount).to eq(total)
+  end
+
+  it 'should decrement total amount of group if deleted' do 
+    Group.destroy_all
+    user = create :user, :email
+    group = create :group, author: user
+    operation1 = create :operation, author: user
+    operation2 = create :operation, author: user
+    total = operation1.amount
+
+    GroupOperation.create( group: group, operation: operation1)
+    destroyable = GroupOperation.create( group: group, operation: operation2)
+
+    GroupOperation.destroy(destroyable.id)
+
+    group = Group.first
+
+    expect(group.total_amount).to eq(total)
+  end
 end
