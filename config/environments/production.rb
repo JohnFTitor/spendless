db24e39d0d9bbcbed0e893321a5a2ede4d6a2601
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-26 15:44:11 -0500

    configure mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'jf-spendless.herokuapp.com', :protocol => 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
