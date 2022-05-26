5168bc3beb91ace0e2fc9405cd13b7539fdc8c06
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-26 12:35:18 -0500

    Configure host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'jf-spendless.herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
