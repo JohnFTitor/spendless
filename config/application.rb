8ac2e7cddb2c6cc4d62761b1eee4befdf96877ed
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-26 15:22:12 -0500

    Change host to use sendgrid

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,7 @@ module Spendless
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     
-    # config.autoload = :classic
-    
     config.autoload_paths += %W(#{config.root}/lib)
-
-    # config/application.rb
-    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
